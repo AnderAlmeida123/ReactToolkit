f4680904281179048a3a3b450387d4f02b6949e6
AnderAlmeida123 <anderson_raft@hotmail.com>
2024-01-16 18:47:32 -0300

    criado utilizando o react redux

--- src/components/product-item/index.jsx
+++ src/components/product-item/index.jsx
@@ -1,7 +1,8 @@
 import { BsCartPlus } from "react-icons/bs";
-
+import { useDispatch } from "react-redux";
 // Components
 import CustomButton from "../custom-button/index";
+import { addProductToCart } from "../../redux/cart/actions";
 
 // Styles
 import * as Styles from "./styles";
@@ -9,10 +10,14 @@ import * as Styles from "./styles";
 // Utilities
 
 const ProductItem = ({ product }) => {
+  const dispatch = useDispatch();
+  const handleProductClick = () => {
+    dispatch(addProductToCart(product));
+  };
   return (
     <Styles.ProductContainer>
       <Styles.ProductImage imageUrl={product.imageUrl}>
-        <CustomButton startIcon={<BsCartPlus />}>
+        <CustomButton startIcon={<BsCartPlus />} onClick={handleProductClick}>
           Adicionar ao carrinho
         </CustomButton>
       </Styles.ProductImage>
